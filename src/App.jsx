2a203ad55dd85173bb5369dcbdd03bb81736d745
Johan <her.1910@hotmail.com>
2024-06-20 22:48:32 -0500

    Se creo Registro,visualizacion y modulacion de proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Conversor from './Conversor'
-
+import Usuarios from './Usuarios'
+import Registro from './Registro'
 function App() {
   const [usuario, setUsuario] = useState('')
   const [clave, setClave] = useState('')
   const [logueado, setLogueado] = useState(false)
+  const [recargar, setRecargar] = useState(false)
+
 
   function cambiarUsuario(evento) {
     setUsuario(evento.target.value)
@@ -17,37 +18,43 @@ function App() {
     setClave(evento.target.value)
   }
 
+  function recargarAhora() {
+    setRecargar(!recargar) 
+    
+  }
+
   async function ingresar() {
-    const peticion = await fetch('http://localhost:3000/login?usuario=' + usuario + '&clave=' + clave, { credentials: 'include'})
+    const peticion = await fetch('http://localhost:3000/login?usuario=' + usuario + '&clave=' + clave, { credentials: 'include' })
     if (peticion.ok) {
       setLogueado(true)
     } else {
       alert('Usuario o clave incorrectos')
-
-
     }
-    // if (usuario == 'admin' && clave == 'admin') {
-    //  alert('Ingresaste')
-    //  setLogueado(true)
-    // } else {
-    // alert('Usuario o clave incorrectos')
-
-    // }
   }
 
-  async function validar(){
+  async function validar() {
     const peticion = await fetch('http://localhost:3000/validar', { credentials: 'include' })
     if (peticion.ok) {
       setLogueado(true)
-    } 
+    }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     validar()
+
   }, [])
-  
+
   if (logueado) {
-    return <Conversor />
+    return (
+
+      <>
+
+        <Registro recargarAhora={recargarAhora} />
+        <Conversor />
+        <Usuarios recargar={recargar} />
+        
+
+      </>)
   }
 
   return (
@@ -56,6 +63,7 @@ function App() {
       < input placeholder='usuario' tipe="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario} />
       <input placeholder='calve' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
       <button onClick={ingresar}>Ingresar</button>
+
 
     </>
   )
